3ca12a5de8bed16d547d1cdbed99b9fea2dfa083
James Perng <jperng@usc.edu>
2014-11-18 18:47:34 -0800

    data members

--- Attack.java
+++ Attack.java
@@ -1,3 +1,7 @@
 public class Attack{
+	private String name;
+	private int damage;
+	private double accuracy;
+	private double crit;
 	
 }
--- Pokemon.java
+++ Pokemon.java
@@ -1,6 +1,11 @@
+import javax.swing.ImageIcon;
+
 public class Pokemon{
 	private String name;
 	private int healthPoints;
+	private Attack[] allAttacks = new Attack[4];
+	private ImageIcon pokemonImage;
+	private double strength;
 	
 	
 }
