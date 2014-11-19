258a4d5c3dfe91790233cbf85f990a1d4553ecb5
David Bell <davidgbe@usc.edu>
2014-11-18 18:46:30 -0800

    updated script

--- migration.sql
+++ migration.sql
@@ -1,10 +1,11 @@
 CREATE TABLE users (
-	ID INTEGER UNIQUE NOT NULL AUTO_INCREMENT,
+	id INTEGER UNIQUE NOT NULL AUTO_INCREMENT,
 	username VARCHAR(30), 
     password VARCHAR(64), 
     wins INTEGER, 
     total_games INTEGER,
     money INTEGER, 
     steroids INTEGER,
-    morphine INTEGER
+    morphine INTEGER,
+    epinephrine INTEGER
 );
