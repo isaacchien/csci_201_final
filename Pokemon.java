9f3d2bbc355eb49102dc116eb5b9a01345ac75cb
Brent Johnson <brentwjo@usc.edu>
2014-11-22 17:45:58 -0800

    added getter + setters

--- Pokemon.java
+++ Pokemon.java
@@ -6,6 +6,36 @@ public class Pokemon{
 	private Attack[] allAttacks = new Attack[4];
 	private ImageIcon pokemonImage;
 	private double strength;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Attack[] getAllAttacks() {
+		return allAttacks;
+	}
+	public void setAllAttacks(Attack[] allAttacks) {
+		this.allAttacks = allAttacks;
+	}
+	public ImageIcon getPokemonImage() {
+		return pokemonImage;
+	}
+	public void setPokemonImage(ImageIcon pokemonImage) {
+		this.pokemonImage = pokemonImage;
+	}
+	public double getStrength() {
+		return strength;
+	}
+	public void setStrength(double strength) {
+		this.strength = strength;
+	}
+	public int getHealthPoints() {
+		return healthPoints;
+	}
+	public void setHealthPoints(int healthPoints) {
+		this.healthPoints = healthPoints;
+	}
 	
 	
 }
